fbe189559227a71dd6c98e054bc8d88d332c0452
MuhammadHaichal <localhostkali06@gmail.com>
2023-11-02 19:35:00 +0700

    membuat aplikasi pertama menggunakan react js sebagai belajar fundamental

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+
+
+function Navbar() {
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Gallery</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar
